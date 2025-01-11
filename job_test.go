8d31c3ac0ffaeab1bbecf1a0603825c5c6b3ab51
Dmitry Soloma <flyingmeatsoul@gmail.com>
2025-01-11 19:09:50 +0300

    job benchmarks

--- job_test.go
+++ job_test.go
@@ -2,6 +2,7 @@ package gojob
 
 import (
 	"context"
+	"log"
 	"testing"
 	"time"
 )
@@ -36,4 +37,40 @@ func TestRunJob(t *testing.T) {
 			t.Fatal(e)
 		}
 	})
+}
+
+// goos: darwin
+// goarch: arm64
+// pkg: github.com/dimonrus/gojob
+// cpu: Apple M2 Max
+// BenchmarkJob_RunAt
+// BenchmarkJob_RunAt-12    	33538953	        33.54 ns/op	       0 B/op	       0 allocs/op
+func BenchmarkJob_RunAt(b *testing.B) {
+	job := NewJob("test.job", func(ctx context.Context, args ...any) error {
+		return nil
+	}, time.Millisecond)
+	g := NewGroup(time.Microsecond, GroupModeConsistently)
+	g.AddJob(job)
+	ctx := context.Background()
+	for i := 0; i < b.N; i++ {
+		now := time.Now()
+		err := job.RunAt(ctx, now)
+		_ = err
+	}
+	b.ReportAllocs()
+}
+
+func BenchmarkGroup_Schedule(b *testing.B) {
+	job := NewJob("test.job", func(ctx context.Context, args ...any) error {
+		return nil
+	}, time.Millisecond)
+	g := NewGroup(time.Microsecond, 3)
+	g.AddJob(job)
+	ctx := context.WithValue(context.Background(), "logger", log.Default())
+	ctx, cancel := context.WithTimeout(ctx, time.Second*5)
+	defer cancel()
+	for i := 0; i < 1; i++ {
+		g.Schedule(ctx)
+	}
+	b.ReportAllocs()
 }
